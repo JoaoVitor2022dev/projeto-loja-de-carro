49c0f299478588199cffb8bff19cf2f6806eb204
Joao Vitor Mocambite Lima <86741407+JoaoVitor2022dev@users.noreply.github.com>
2022-08-29 17:20:17 -0400

    Create main.ts

---
+++ poo-final/main.ts
@@ -0,0 +1,16 @@
+import Carro from "./carro";
+import Cliente from "./cliente";
+import CompraCarro from "./comprarCarro";
+import Funcionario from "./funcionario";
+
+let car = new Carro('Logan' , 1970 ,  10000); 
+
+let cliente = new Cliente('Diego' , 4999); 
+
+let funcionario = new Funcionario('joao'); 
+
+let compra = new CompraCarro(cliente, funcionario , car); 
+
+compra.detalheCompra(); 
+ 
+compra.showQuestion();
