217d5e9dcf6640fbdd2be4a25d460ad365b2338f
Joao Vitor Mocambite Lima <86741407+JoaoVitor2022dev@users.noreply.github.com>
2022-08-31 10:43:36 -0400

    Update comprarCarro.ts

--- poo-final/comprarCarro.ts
+++ poo-final/comprarCarro.ts
@@ -21,13 +21,13 @@
 
    private darDesconto(preco: number) {
        if(this.carro.getAno() < 2000 ){
-         this.valorComDesconto = this.carro.getValor() * 90; 
+         this.valorComDesconto = this.carro.getValor() *0.90; 
          this.carro.setValor(this.valorComDesconto); 
        } else {
         this.carro.setValor(preco)
        }
        if (this.cliente.getRenda() < 5000) {
-         this.valorComDesconto = this.carro.getValor() * 90; 
+         this.valorComDesconto = this.carro.getValor() * 0.90; 
          return this.carro.setValor(this.valorComDesconto);  
        } else {
          return this.carro.setValor(this.carro.getValor()); 
